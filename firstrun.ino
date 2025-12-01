72414a4abc812193058fb9217ede94874a84a2ab
SANKAP_kalode <110105130+sankalpkalode49@users.noreply.github.com>
2025-12-01 12:10:32 +0530

    Refactor motor control code for clarity and efficiency

--- firstrun.ino
+++ firstrun.ino
@@ -1,57 +1,48 @@
 #include "CytronMotorDriver.h"
 
+// Configure Cytron Motor Driver: PWM_DIR mode, PWM pin, DIR pin
 CytronMD motor1(PWM_DIR, 5, 4);
 CytronMD motor2(PWM_DIR, 9, 6);
-char sleep_status = 0;
+
+char command = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600); // Set monitor baudrate to 9600
-
+  // Initialize Serial Monitor at 9600 baud
+  Serial.begin(9600); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-   while (Serial.available() > 0) 
+  // Check if data is available to read
+  while (Serial.available() > 0) 
   {
-    sleep_status = Serial.read();
-    if(sleep_status == 'r')
+    command = Serial.read();
+    
+    // 'r' = Rotate Right / Turn Right
+    if(command == 'r')
     { 
         motor1.setSpeed(60); 
         motor2.setSpeed(-(60)); 
-        Serial.write("LEFT");
+        Serial.write("LEFT"); // Debug response
     }
-    else if(sleep_status == 'f')
+    // 'f' = Move Forward
+    else if(command == 'f')
     {
-       
-      
         motor1.setSpeed(60); 
         motor2.setSpeed(60); 
         Serial.write("FORWARD");
     }
-    else if(sleep_status == 'l')
+    // 'l' = Rotate Left / Turn Left
+    else if(command == 'l')
     {
-       
-      
         motor1.setSpeed(-(60)); 
         motor2.setSpeed(60); 
         Serial.write("RIGHT");
     }
-//    else if(sleep_status == 'o')
-//    {
-//        motor1.setSpeed(-(60)); 
-//        motor2.setSpeed(60); 
-//        delay(1000);
-//        motor1.setSpeed(60); 
-//        motor2.setSpeed(-(60));
-//        
-//      
-//    }
+    // Stop if any other command or explicit stop is received
     else
     {
       motor1.setSpeed(0); 
       motor2.setSpeed(0);
     }
   }
-
 }
